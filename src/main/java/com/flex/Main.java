cf8becde954b5f14b4ecc50aa448cac505315231
Vincent <vincent.burkhardt.home@gmail.com>
2024-01-01 17:02:52 +0100

    fixed new ENV variables

--- src/main/java/com/flex/Main.java
+++ src/main/java/com/flex/Main.java
@@ -15,24 +15,24 @@ public class Main {
 
         MySqlController database = new MySqlController();
         database.connect(
-                System.getenv("FLEX_DB_URL"),
-                System.getenv("FLEX_DB_USERNAME"),
-                System.getenv("FLEX_DB_PASSWORD")
+                System.getenv("MYSQL_DB_URL"),
+                System.getenv("MYSQL_DB_USERNAME"),
+                System.getenv("MYSQL_DB_PASSWORD")
         );
         AccessTokenProvider provider = new AccessTokenProvider(
-                System.getenv("FLEX_CLIENT_ID"),
-                System.getenv("FLEX_CLIENT_SECRET"),
+                System.getenv("OSU_CLIENT_ID"),
+                System.getenv("OSU_CLIENT_SECRET"),
                 database.getConnection(),
                 HttpClient.newHttpClient()
         );
         provider.obtainAccessToken();
 
-        Bot bot = new Bot(System.getenv("FLEX_DISCORD_TOKEN"), database.getConnection());
+        Bot bot = new Bot(System.getenv("DISCORD_TOKEN"), database.getConnection());
         Flex flex = new Flex(bot.getApi(), database.getConnection());
         while(true){
             flex.start();
             try {
-                Thread.sleep(30000);
+                Thread.sleep(60000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
