904f749066a2f9fad384ec8b492b47628b7b1a7d
Vincent <vincent.burkhardt.home@gmail.com>
2024-01-09 23:12:54 +0100

    moved ScoreEmbedData to just one class

--- src/main/java/com/flex/discord/embeds/ScoreEmbed.java
+++ src/main/java/com/flex/discord/embeds/ScoreEmbed.java
@@ -1,38 +1,152 @@
 package com.flex.discord.embeds;
 
-import com.flex.discord.entities.ScoreEmbedData;
+import com.flex.data.FlexData;
+import com.flex.osu.api.requests.utility.ColorFinder;
 import com.flex.osu.entities.OsuData;
 import com.flex.osu.entities.score.Score;
 import lombok.Getter;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 
+import java.awt.*;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.time.Duration;
 import java.time.Instant;
 
 public class ScoreEmbed {
 
-    private final ScoreEmbedData data;
-
     @Getter
     private MessageEmbed embed;
+    private final Score score;
+    private String title;
+    private String mods;
+    private String rating;
+    private String bpm;
+    private String length;
+    private String rank;
+    private String accuracy;
+    private String statistics;
+    private String pp;
+    private String scoreUrl;
+    private Color color;
+    private String author;
+    private String userUrl;
+    private String avatar;
+    private String version;
+    private String cover;
+    private final int scoreIndex;
 
     public ScoreEmbed(OsuData data) {
-        this.data = new ScoreEmbedData(data);
+        score = data.getScore();
+        scoreIndex = data.getScoreIndex();
+        setDefaults();
         embed = createEmbed();
     }
 
     private MessageEmbed createEmbed() {
         return new EmbedBuilder()
-                .setTitle(data.getTitle())
-                .setDescription(data.getMods() + " at **" + data.getRating() + "** :star: - " + data.getBpm() + " bpm - " + data.getLength() + "\n" +
-                        "**" + data.getRank() + "** Rank (" + data.getAccuracy() + "%) - " + data.getStatistics() + "\n" +
-                        "**" + data.getPp() + " pp**")
-                .setUrl(data.getScoreUrl())
-                .setColor(data.getColor())
-                .setAuthor(data.getAuthor(), data.getUserUrl(), data.getAvatar())
-                .setFooter(data.getVersion())
+                .setTitle(title)
+                .setDescription(mods + " at **" + rating + "** :star: - " + bpm + " bpm - " + length + "\n" +
+                        "**" + rank + "** Rank (" + accuracy + "%) - " + statistics + "\n" +
+                        "**" + pp + " pp**")
+                .setUrl(scoreUrl)
+                .setColor(color)
+                .setAuthor(author, userUrl, avatar)
+                .setFooter(version)
                 .setTimestamp(Instant.now())
-                .setImage(data.getCover())
+                .setImage(cover)
                 .build();
+    }
+
+    private void setDefaults() {
+        setTitle();
+        setLength();
+        setMods();
+        setRating();
+        setBpm();
+        setRank();
+        setAccuracy();
+        setStatistics();
+        setPp();
+        setScoreUrl();
+        setColor();
+        setAuthor();
+        setUserUrl();
+        setAvatar();
+        setVersion();
+        setCover();
+    }
+
+
+    private void setTitle() {
+        title = score.beatmapset.artist + " - " + score.beatmapset.title + " [" + score.beatmap.version + "]";
+    }
+
+    private void setLength() {
+        Duration duration = Duration.ofSeconds(score.beatmap.total_length);
+        length = String.format("%d:%02d", duration.toMinutes(), duration.toSecondsPart());
+    }
+
+    private void setMods() {
+        mods = score.mods.isEmpty() ? "NM" : String.join("", score.mods);
+    }
+
+    private void setRating() {
+        rating = String.valueOf(score.beatmap.difficulty_rating);
+    }
+
+    private void setBpm() {
+        bpm = String.valueOf(score.beatmap.bpm);
+    }
+
+    private void setRank() {
+        rank = score.rank;
+    }
+
+    private void setAccuracy() {
+        DecimalFormat decimalFormat = new DecimalFormat("0.00");
+        accuracy = decimalFormat.format(score.accuracy * 100);
+    }
+
+    private void setStatistics() {
+        statistics = "[" + score.statistics.count_300 + "/" + score.statistics.count_100
+                + "/" + score.statistics.count_50 + "/" + score.statistics.count_miss + "]";
+    }
+
+    private void setPp() {
+        pp = String.valueOf(Math.round(score.pp));
+    }
+
+    private void setScoreUrl() {
+        scoreUrl = "https://osu.ppy.sh/scores/osu/" + score.best_id;
+    }
+
+    private void setColor() {
+        try {
+            color = new ColorFinder().getDominantColor(score.user.avatar_url);
+        } catch (IOException e) {
+            color = FlexData.getRandomOsuPaletteColor();
+        }
+    }
+
+    private void setAuthor() {
+        author = String.format("New #%s top play for %s", scoreIndex, score.user.username);
+    }
+
+    private void setUserUrl() {
+        userUrl = "https://osu.ppy.sh/users/" + score.user.id;
+    }
+
+    private void setAvatar() {
+        avatar = score.user.avatar_url;
+    }
+
+    private void setVersion() {
+        version = FlexData.CLIENT_VERSION;
+    }
+
+    private void setCover() {
+        cover = score.beatmapset.covers.cover;
     }
 }
